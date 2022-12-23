6a6f927a393bf44af3d94fd45d2eadf4322533f2
obedsarpeh <obedsarpeh1@rocketmail.com>
2022-12-23 17:55:42 +0000

    correction

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 character = ""
 for alphabet in range(97, 123):
-    character = str(chr(alphabet))
-    print(f"{character}", end="")
+    character = chr(alphabet)
+    print(f"{character}".format(str(character)), end="")
