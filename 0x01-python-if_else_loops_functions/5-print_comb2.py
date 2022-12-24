8a9a4f639ed7dae0baef54544d35a9a9028bcf41
obedsarpeh <obedsarpeh1@rocketmail.com>
2022-12-24 08:48:18 +0000

    good to go

---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+for number in range(0, 100):
+    if number < 10:
+        print("{}".format('0' + str(number)), end=", ")
+#    elif number >= 10 and number <= 99:
+#       if number == 99:
+#            break;
+    elif number >= 10 and number <= 98:
+        print("{}".format(number), end=", ")
+    if number == 99:
+        print("{}".format(number))
