7cf1f57dccbc4dd2239dd2f81a404917a5f36788
obedsarpeh <obedsarpeh1@rocketmail.com>
2022-12-27 18:48:06 +0000

    correction

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,5 +4,5 @@ if __name__ == "__main__":
 
     newsum = 0
     for i in range(len(sys.argv) - 1):
-        newsum += int(sys.argv[i+ 1])
+        newsum += int(sys.argv[i + 1])
     print("{}".format(newsum))
