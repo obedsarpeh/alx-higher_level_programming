eddbc05c809defae7a49eb7828463c8004252586
obedsarpeh <obedsarpeh1@rocketmail.com>
2023-01-16 03:30:09 +0000

    correction

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,4 +1,3 @@
 def no_c(my_string):
-    for idx in range(len(my_string)):
-        for c in my_string:
-            if my_string[idx]:
+    new = [x for x in my_string if x != 'c' and x != 'C']
+    return("".join(new))
