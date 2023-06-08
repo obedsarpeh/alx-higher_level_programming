632e66ff752be7e3e23868cf533b1cddb005ad09
obedsarpeh <obedsarpeh1@rocketmail.com>
2023-06-08 04:11:46 +0000

    conquering advanced task my priority

---
+++ 0x00-python-hello_world/100-write.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# This program is a python script that
+# that prints a given text by using the write
+# function from the sys module without using print.
+import sys
+sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
