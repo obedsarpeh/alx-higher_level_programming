b0281c45113be5f96008fc0fe40d7bd4c3e38b2b
obedsarpeh <obedsarpeh1@rocketmail.com>
2023-06-09 03:17:12 +0000

    change is constant

--- new/3-infinite_add.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
-
-    newsum = 0
-    for i in range(len(sys.argv) - 1):
-        newsum += int(sys.argv[i + 1])
-    print("{}".format(newsum))
