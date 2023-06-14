ab4612fa4cbd2b9083d7874193e7fd6639cfc54f
obedsarpeh <obedsarpeh1@rocketmail.com>
2023-06-14 04:20:22 +0000

    more on modules

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,5 +4,16 @@ if __name__ == "__main__":
     import sys
     count = len(sys.argv) - 1
     if count != 3:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>\n")
-        exit(1) 
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+    
+    operators = {"+": add, "-": sub, "*": mul, "/": div}
+    if sys.argv[2] not in list(operators.keys()):
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    print("{} {} {} = {}".format(a, sys.argv[2], b, operators[sys.argv[2]](a, b)))
+
+
