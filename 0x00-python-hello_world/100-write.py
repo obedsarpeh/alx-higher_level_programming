fb2baef0c7f45f858001f0446080c4aba03ee6f0
obedsarpeh <obedsarpeh1@gmail.com>
2023-12-07 00:50:51 +0000

    added a newline to correct previous error

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -3,4 +3,4 @@
 # that prints a given text by using the write
 # function from the sys module without using print.
 import sys
-sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19")
+sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
