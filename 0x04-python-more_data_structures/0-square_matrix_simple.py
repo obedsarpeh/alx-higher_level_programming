4a887df8298e3e861e38e4494f32610aa15f64b0
obedsarpeh <obedsarpeh1@gmail.com>
2023-12-11 14:32:49 +0000

    at long last

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-   # return ([list(map(lambda y: y*y, row)) for row in matrix])
-   return [[x * x for x in row] for row in matrix]
+    # return ([list(map(lambda y: y*y, row)) for row in matrix])
+    return [[x * x for x in row] for row in matrix]
