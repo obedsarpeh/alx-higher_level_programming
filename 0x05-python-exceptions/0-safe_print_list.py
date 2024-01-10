877304a59935c3ee6fe93c7ca6ecf4782f7937ee
obedsarpeh <obedsarpeh1@gmail.com>
2024-01-10 03:02:10 +0000

    task-0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -9,6 +9,11 @@ Return:
 def safe_print_list(my_list=[], x=0):
     res = 0
     for idx in range(x):
-        print("{}".format(my_list[i]), end="")
-        res +=1
+        try:
+            print("{}".format(my_list[i]), end="")
+            res +=1
+        except IndentError:
+            break
+        print("")
+
     return(res)
