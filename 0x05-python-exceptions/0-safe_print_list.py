0a608deffadc07e4406aff530f8552d4c5a43aad
obedsarpeh <obedsarpeh1@gmail.com>
2024-01-10 02:58:13 +0000

    task-0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!usr/bin/python3
+'''
+Args:
+    my_list: list of type int, string, etc.
+    x: number of items in my_list to be printed
+Return:
+    return number of items printed
+'''
+def safe_print_list(my_list=[], x=0):
+    res = 0
+    for idx in range(x):
+        print("{}".format(my_list[i]), end="")
+        res +=1
+    return(res)
