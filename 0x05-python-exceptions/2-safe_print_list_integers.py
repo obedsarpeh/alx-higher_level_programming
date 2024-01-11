bbe0d6803614d9c988edc03879ec6db67e5f532f
obedsarpeh <obedsarpeh1@gmail.com>
2024-01-11 22:28:40 +0000

    skipping non-integers

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+'''
+Args:
+    my_list: list of type int, string, etc.
+    x: number of items in my_list to be printed
+Return:
+    return number of items printed
+'''
+
+
+def safe_print_list_integers(my_list=[], x=0):
+
+    count = 0
+
+    try:
+        for idx in range(x):
+            try:
+                integer_value = int(my_list[idx])
+                print("{:d}".format(my_list[idx]), end="")
+                count += 1
+            except (TypeError, ValueError):
+                pass
+    finally:
+        print()
+    return (count)
