036faac444d639bd1a0d325f7431102f73490f89
obedsarpeh <obedsarpeh1@gmail.com>
2024-01-11 21:41:24 +0000

    tests are always important

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -11,7 +11,8 @@ def safe_print_integer(value):
 
     try:
         print("{:d}".format(value))
+        return (True)
     except (TypeError, ValueError):
-        return False
+        return (False)
 
     return (value)
