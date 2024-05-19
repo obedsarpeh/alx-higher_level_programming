6694feb21af3d259536349e256ae2b72f21e548a
obedsarpeh <obedsarpeh1@gmail.com>
2024-05-19 20:33:20 +0000

    A class is an instance

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+0-square.py
+
+This is a module that seeks to find the size of a square
+"""
+
+
+class Square:
+
+    """
+    An class with a private instance attribute 'size'
+    """
+
+    def __init__ (self, value):
+        self.__value = value
+
