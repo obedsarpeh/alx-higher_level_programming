d270910f877744b0a477b73c111825b7ecc795b7
obedsarpeh <obedsarpeh1@gmail.com>
2024-05-19 19:57:13 +0000

    empty class

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+"""
+0-square.py
+
+This is a module that seeks to find the size of a square
+"""
 class Square:
-    ''' An empty class to calculate a square '''
+    """
+    An empty class to calculate a square
+    """
     pass
