b084cbabf66f9374c1e752653c935cec3a109410
obedsarpeh <obedsarpeh1@gmail.com>
2024-05-28 03:12:22 +0000

    grant select

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -3,6 +3,6 @@
 -- The user_0d_2 password should be set to user_0d_2_pwd
 -- If the database hbtn_0d_2 already exists, your script should not fail
 -- If the user user_0d_2 already exists, your script should not fail
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED WITH mysql_native_password BY 'Kokonsah@17';
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED WITH mysql_native_password BY 'user_0d_2_pwd';
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
