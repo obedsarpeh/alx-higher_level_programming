82bf83926d546b50f12e15a3d6a754759f69933f
obedsarpeh <obedsarpeh1@gmail.com>
2024-08-01 19:48:45 +0000

    fixed checker error

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -3,10 +3,10 @@
 -- Results must be sorted in ascending order by cities.id
 -- You can use only one SELECT statement
 -- The database name will be passed as an argument of the mysql command
-SELECT cities.id, cities.name, (
-	SELECT states.name
+SELECT id, name, (
+	SELECT name
 	FROM states
 	WHERE states.id = cities.state_id
-) AS state_name
+)
 FROM cities
 ORDER BY cities.id ASC;
