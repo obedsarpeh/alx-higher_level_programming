1dc20e6d77580fdf118c271fe7d5edd3aa4772f5
obedsarpeh <obedsarpeh1@gmail.com>
2024-08-02 03:44:42 +0000

    no genre

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -16,11 +16,14 @@ tv_show_genres.genre_id
 FROM
 tv_shows
 
-RIGHT JOIN
+LEFT JOIN
 tv_show_genres
 
 ON
 tv_shows.id = tv_show_genres.show_id
+
+WHERE
+tv_show_genres.show_id is NULL
 
 ORDER BY
 tv_shows.title ASC,
