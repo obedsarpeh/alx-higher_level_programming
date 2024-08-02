ca37436dd38cedbc8f8ffc7576ade99d9824d493
obedsarpeh <obedsarpeh1@gmail.com>
2024-08-02 06:00:53 +0000

    fixed error

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,17 +1,9 @@
 -- Convert the database to UTF8MB4
--- ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- Convert the table to UTF8MB4
--- ALTER TABLE hbtn_0c_0.first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- Convert the name field to UTF8MB4
--- ALTER TABLE hbtn_0c_0.first_table MODIFY COLUMN name VARCHAR(256) CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;
--- Convert the database to UTF8MB4
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- Convert the table to UTF8MB4
 ALTER TABLE hbtn_0c_0.first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- Convert the name field to UTF8MB4
-ALTER TABLE hbtn_0c_0.first_table MODIFY COLUMN name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE hbtn_0c_0.first_table MODIFY COLUMN name VARCHAR(256) COLLATE utf8mb4_unicode_ci;
 
