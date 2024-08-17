c1e43c32154ad3aad215333f243953e3f6edeb1b
Obed Sarpeh <obedsarpeh1@rocketmail.com>
2024-08-17 04:57:12 +0000

    size validation

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+0-square.py
+
+This is a module that seeks to find the size of a square
+"""
+
+
+class Square:
+
+    """
+    A class with a private instance attribute 'size'
+    """
+    def __init__(self, size=0):
+
+        """
+        Initializes a private size attribute
+
+        Args:
+            size (int): The size of the square (optional, defaults to 0).
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        if not isinstance(size, int):
+            raise TypeError("must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
