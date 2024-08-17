675ca11a93a1734777030a07193543e63476bf35
Obed Sarpeh <obedsarpeh1@rocketmail.com>
2024-08-17 04:36:02 +0000

    square size

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -9,12 +9,11 @@ This is a module that seeks to find the size of a square
 class Square:
 
     """
-    An class with a private instance attribute 'size'
+    A class with a private instance attribute 'size'
     """
+    def __init__(self, size):
 
-    def __init__(self, value):
         """
         Initializes a private attribute
         """
-
-        self.__value = value
+        self.__size = size
